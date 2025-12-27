a3551a297132a7b46575926c6170085ea931a0d4
shubhamjadhav0715 <shubhamjadhav0715@gmail.com>
2025-12-27 13:38:58 +0530

    Remove all comments from User.js model

--- backend/models/User.js
+++ backend/models/User.js
@@ -59,7 +59,6 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 });
 
-// Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
     next();
@@ -68,7 +67,6 @@ userSchema.pre('save', async function(next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// Compare password method
 userSchema.methods.comparePassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
